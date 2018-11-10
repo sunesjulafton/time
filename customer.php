da9bceaccbc6a6ff33fa454e9111811a49404164
Robert Dahlin <sunesjulafton@gmail.com>
2018-11-10 23:18:10 +0100

    Prepared customer page

--- customer.php
+++ customer.php
@@ -1,6 +1,12 @@
 <?php 
 
+	include 'core/init.php';
+	$user_id = $_SESSION['user_id'];
+	$user = $getFromU->userData($user_id);
 	
+	if($getFromU->loggedIn() === false) {
+		header('Location: index.php');
+	}
 
 
 ?>
