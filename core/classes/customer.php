d4dba01a31c5b890ed261f6611bbbf7226108b81
Robert Dahlin <sunesjulafton@gmail.com>
2018-11-11 17:26:43 +0100

    yrd

--- core/classes/customer.php
+++ core/classes/customer.php
@@ -13,5 +13,29 @@
 			return $stmt->fetch(PDO::FETCH_OBJ);
 		}
 
+
+		public function register_support($username, $email, $password) {
+			
+			$password = password_hash($password, PASSWORD_DEFAULT);
+
+			
+
+			$stmt = $this->pdo->prepare('INSERT INTO users (username, email, password) VALUES (?, ?, ?)');
+			
+			$stmt->bindParam(1, $username, PDO::PARAM_STR);
+			$stmt->bindParam(2, $email, PDO::PARAM_STR);
+			$stmt->bindParam(3, $password, PDO::PARAM_STR);
+			
+			$stmt->execute();
+
+			$user_id = $this->pdo->lastInsertId();
+
+			return $_SESSION['user_id'] = $user_id;
+
+
+		}
+
+
+
 	}
 ?>
